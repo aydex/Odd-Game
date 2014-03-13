85eeb7302359685ae5b5e21b5600ef9557e836f7
Aydex <adrian.hundseth@hotmail.com>
2014-03-13 15:36:13 +0100

    Odd

---
+++ src/test/Odd.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Odd {
+	
+	Odd() {
+		for (int i = 0; i >= 0; i++) {
+			System.out.println("Odd");
+		}				
+	}
+	
+	public static void main(String[] args) {
+		Odd odd = new Odd();
+	}
+}
