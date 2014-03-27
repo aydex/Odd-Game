29ec2b11e4a69cec5d991b08bdf6dc8cb83c801c
Capeo <odd.cappelen@gmail.com>
2014-03-27 18:26:23 +0100

    Continued work on combat class and graphics

--- src/combat/CombatGraphics.java
+++ src/combat/CombatGraphics.java
@@ -2,7 +2,9 @@ package combat;
 
 
 import java.net.URL;
-
+import party.Party;
+import party.FriendlyParty;
+import party.EnemyParty;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,6 +16,15 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class CombatGraphics extends Application{
+	
+	private Button enemy0;
+	private Button enemy1;
+	private Button enemy2;
+	private Button enemy3;
+	
+	CombatGraphics(Stage stage, FriendlyParty party, EnemyParty enemy){
+		Combat combat = new Combat(party,enemy);
+	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
